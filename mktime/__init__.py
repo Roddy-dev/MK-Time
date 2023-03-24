e949369641c9396e2ae3d654cdccfe5cb1adb4c8
Roddy-dev <62653018+Roddy-dev@users.noreply.github.com>
2023-03-24 11:31:39 +0000

    new namespace mktime and working home page

---
+++ mktime/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some-long-secret-to-be-added' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #login template
+login_manager.login_message_category = 'info' #bootstrap colour of message
+
+
+from mktime import routes
+
---
+++ run.py
@@ -0,0 +1,5 @@
+from mktime import app
+
+if __name__ == '__main__':
+    app.run(debug=True) # allows us to run script from python and set env variables
+                        # in this case python flaskblog.py otherwise use flask run
